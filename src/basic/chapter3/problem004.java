005fb196793f43a2affbc334ecc002d2abdc904a
syr <blossomy1d@gmail.com>
2024-03-16 16:06:17 +0900

    update problem004

--- src/basic/chapter3/problem004.java
+++ src/basic/chapter3/problem004.java
@@ -16,24 +16,16 @@ public class problem004 {
 
         //n 범위 체크
         if (n >= 1 && n <= 1024) {
-            long[][] N = new long[n][n];
-            long[][] S = new long[n][n];
-            for (int i = 0; i < n; i++) {
+            long[][] N = new long[n + 1][n + 1];
+            long[][] S = new long[n + 1][n + 1];
+            for (int i = 1; i <= n; i++) {
                 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                for (int j = 0; j < n; j++) {
+                for (int j = 1; j <= n; j++) {
                     //n*n 표에 숫자를 하나씩 채워넣기
-                    N[i][j] = N[i][j] + Integer.parseInt(stringTokenizer.nextToken());
+                    N[i][j] = Integer.parseInt(stringTokenizer.nextToken());
 
                     //구간합 구하기
-                    if (i == 0 && j == 0) {
-                        S[i][j] = N[i][j];
-                    } else if (i == 0) {
-                        S[i][j] = N[i][j] + S[i][j - 1];
-                    } else if (j == 0) {
-                        S[i][j] = N[i][j] + S[i - 1][j];
-                    } else {
-                        S[i][j] = N[i][j] + S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1];
-                    }
+                    S[i][j] = N[i][j] + S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1];
                 }
             }
 
@@ -42,24 +34,16 @@ public class problem004 {
                 for (int i = 0; i < m; i++) {
                     stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-                    //(x1, y1)부터 (x2, y2)까지의 합 출력하기 위해 정수 4개 입력. 이때 배열이므로 1씩 빼준 것 !
-                    int x1 = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-                    int y1 = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-                    int x2 = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-                    int y2 = Integer.parseInt(stringTokenizer.nextToken()) - 1;
+                    //(x1, y1)부터 (x2, y2)까지의 합 출력하기 위해 정수 4개 입력.
+                    int x1 = Integer.parseInt(stringTokenizer.nextToken());
+                    int y1 = Integer.parseInt(stringTokenizer.nextToken());
+                    int x2 = Integer.parseInt(stringTokenizer.nextToken());
+                    int y2 = Integer.parseInt(stringTokenizer.nextToken());
 
                     //x1, x2, y1, y2 범위 체크
                     if (x1 <= x2 && x2 < 1000 && y1 <= y2 && y2 < 1000) {
                         //표 생각 ! 전체 구간합 - 제외된 위 가로 구간합 - 제외된 왼 세로 구간합 + 중복으로 빠진 구간합
-                        if (x1 == 0 && y1 == 0) {
-                            System.out.println(S[x2][y2]);
-                        } else if (x1 == 0 && y1 > 0) {
-                            System.out.println(S[x2][y2] - S[x2][y1 - 1]);
-                        } else if (x1 > 0 && y1 == 0) {
-                            System.out.println(S[x2][y2] - S[x1 - 1][y2]);
-                        } else {
-                            System.out.println(S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1]);
-                        }
+                        System.out.println(S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1]);
                     } else {
                         System.out.println("x1, x2, y1, y2의 범위를 확인해주세요.");
                     }
